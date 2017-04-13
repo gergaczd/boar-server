ee827b6616bb755233c0c01c69966ea33e574c4a
gergaczd <gergaczdaniel@gmail.com>
2017-04-13 17:03:02 +0200

    feat(app): remove unused features

--- app/index.js
+++ app/index.js
@@ -1,14 +1,8 @@
 'use strict';
 
 let fs = require('fs');
-let http = require('http');
-let https = require('https');
-let serve = require('koa-static');
 let cors = require('kcors');
-let Pug = require('koa-pug');
-let errorHandlerMiddleware = require('./middlewares/error-handler');
 let methodOverride = require('koa-methodoverride');
-let HookMiddlewareFactory = require('./middlewares/hook');
 let bodyparser = require('koa-bodyparser');
 let requestId = require('koa-requestid');
 let ssl = require('koa-ssl');
@@ -36,41 +30,12 @@ class App {
     }.bind(this));
   }
 
-  loadModels(path) {
-    fs.readdirSync(path).forEach(function(file) {
-      if (/(.*)\.(js$)/.test(file) && !/(.*)\.(spec.js$)/.test(file)) {
-        require(path + '/' + file);
-      }
-    }.bind(this));
-  }
-
   addMiddleware(middleware) {
     this.koaApp.use(middleware);
   }
 
-  addStaticContentMiddleware(path) {
-    this.addMiddleware(serve(path));
-  }
-
-  addDynamicViewMiddleware(root, cache) {
-    let pugMiddleware = new Pug({
-      viewPath: root,
-      noCache: !cache
-    });
-
-    pugMiddleware.use(this.koaApp);
-  }
-
-  addHookMiddleware() {
-    this.addMiddleware(HookMiddlewareFactory.getMiddleware());
-  }
-
   addMethodOverrideMiddleware(fieldName) {
     this.addMiddleware(methodOverride(fieldName));
-  }
-
-  addErrorHandlerMiddleware(renderPath) {
-    this.addMiddleware(errorHandlerMiddleware(renderPath));
   }
 
   addBodyParseMiddleware(options) {
@@ -90,32 +55,6 @@ class App {
 
   addEnforceSSLMiddleware(options) {
     this.addMiddleware(ssl(options));
-  }
-
-  listen(port, env) {
-    let httpPort = parseInt(port);
-    this._startHTTPServer(httpPort, env);
-
-    if (process.env.SERVE_HTTPS === 'true') {
-      let httpsPort = httpPort + 10000;
-      this._startHTTPSServer(httpsPort, env);
-    }
-  }
-
-  _startHTTPServer(port, env) {
-    http.createServer(this.koaApp.callback()).listen(port);
-    console.log('Application started:', { port: port, env: env });
-  }
-
-  _startHTTPSServer(port, env) {
-    let httpsOptions = {};
-    if (process.env.HTTPS_KEY && process.env.HTTPS_CERT) {
-      httpsOptions.key = fs.readFileSync(process.env.HTTPS_KEY);
-      httpsOptions.cert = fs.readFileSync(process.env.HTTPS_CERT);
-    }
-
-    https.createServer(httpsOptions, this.koaApp.callback()).listen(port);
-    console.log('Application started (with SSL):', { port: port, env: env });
   }
 }
 
--- index.js
+++ index.js
@@ -4,18 +4,8 @@ module.exports = {
 
   app: require('./app'),
 
-  middlewares: {
-    hook: require('./app/middlewares/hook')
-  },
-
   lib: {
     controllerFactory: require('./lib/controller-factory'),
-    realIpAddress: require('./lib/real-ip-address'),
-    Database: require('./lib/database'),
-    maskEmailAddress: require('./lib/mask-email-address'),
-    exceptionHandler: require('./lib/exception-handler'),
-    clearCollections: require('./lib/clear-collections'),
-    clearGridfs: require('./lib/clear-gridfs'),
     securityMiddlewareFactory: require('./lib/security-middleware-factory')
   }
 
